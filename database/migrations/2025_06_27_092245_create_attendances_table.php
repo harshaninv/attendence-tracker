a7e65c5c7de2ae23ae120fdf00ea702dca1a8a3f
Harsh0726 <harshanineranjana@gmail.com>
2025-06-27 16:02:28 +0530

    created attenances table

---
+++ database/migrations/2025_06_27_092245_create_attendances_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
+            $table->foreignId('subject_id')->constrained('subjects')->onDelete('cascade');
+            $table->date('attendance_date');
+            $table->enum('status', ['present','absent'])->default('absent');
+            $table->foreignId('recorded_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->unique(['student_id', 'subject_id', 'attendance_date']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
