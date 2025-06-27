971250e4f27d072ae15c1583327337ad47e59255
Harsh0726 <harshanineranjana@gmail.com>
2025-06-27 16:30:07 +0530

    Created model for student and relationship funtions

---
+++ app/Models/Student.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'registration_number',
+        'first_name',
+        'last_name',
+        'email',
+    ];
+
+    //many to many relationship with subjects through student_subject pivot table
+    public function subjects(){
+        return $this->belongsToMany(Subject::class, 'student_subject', 'student_id', 'subject_id')
+                    ->withPivot('enrollment_date')
+                    ->withTimestamps();
+    }
+
+    //reltionship with attendences
+    public function attendances(){
+        return $this->hasMany(Attendance::class);
+    }
+}
