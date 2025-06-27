78b7df4bb03bb94e6733da98e4778cba3de064be
Harsh0726 <harshanineranjana@gmail.com>
2025-06-27 22:57:13 +0530

    fixed column name

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -10,7 +10,7 @@ class Student extends Model
     use HasFactory;
 
     protected $fillable = [
-        'registration_number',
+        'registraion_number',
         'first_name',
         'last_name',
         'email',
